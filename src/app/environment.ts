150f4f56897ef3ebcb434211c4cf906b1709f25f
ruedagato <ruedagato@gmail.com>
2016-12-01 19:00:51 -0500

    environment add

---
+++ src/app/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+	production: false,
+	config: {
+		apiKey: "AIzaSyDCj0Cim15TsLNHCXghpzUdx0574h9vygg",
+		authDomain: "trepsi-prod.firebaseapp.com",
+		databaseURL: "https://trepsi-prod.firebaseio.com",
+		storageBucket: "trepsi-prod.appspot.com",
+		messagingSenderId: "733165306067"
+	}
+};
